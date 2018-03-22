7df2f0c3571b22c2f0a158d99d14eab4a1912e81
cataire <mail.for.mu@gmail.com>
2018-03-21 18:12:58 -0600

    bringing up to date

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "tWister@710",
-  database: "burger_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+   port: 3306,
+   host: "localhost",
+   user: "root",
+   password: "tWister@710",
+   database: "burgers_db"
+ });
+}
+
 
 connection.connect(function(err) {
   if (err) {
